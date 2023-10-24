31b98e87c04efa67d398882d8ebbf21fe0a11ff9
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-10-24 13:31:21 +0200

    added scroll snap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import "./app.scss"
+
 const App = () => {
   return <div>
-    
+    <section>hero</section>
+    <section>parallax</section>
+    <section>services</section>
+    <section>parallax</section>
+    <section>portfolio1</section>
+    <section>portfolio2</section>
+    <section>portfolio3</section>
+    <section>contact</section>
   </div>;
 };
 
 export default App;
+
